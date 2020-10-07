d0800b4fc2f66376f69d4b273d14627e372d77c4
Dendrocopos-kr <lunatic787973@gmail.com>
2020-10-07 17:48:02 +0900

    오류 찾아야함.오토와이어드 연결 안됨

--- src/main/java/com/amolrang/modume/config/SecurityConfig.java
+++ src/main/java/com/amolrang/modume/config/SecurityConfig.java
@@ -1,20 +1,34 @@
 package com.amolrang.modume.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+
+import com.amolrang.modume.model.CustomUser;
+import com.amolrang.modume.model.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
 	}
 
 	@Override
@@ -22,7 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		String[] guest = {"/","/main","/login","/join"};
 		String loginPage = "/login";
 		String loginProcessingUrl = "/loginAction";
-		String logoutUrl = "/logout";
 		String mainPage = "/";
 		
 		http
--- src/main/java/com/amolrang/modume/model/MemberMapper.java
+++ src/main/java/com/amolrang/modume/model/MemberMapper.java
@@ -5,9 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
     MemberDTO chkLogin(String user_email);
-
     int insertMember(MemberDTO memberDTO);
-
-    // 로그아웃-토큰삭제
     int deletePersistentLogins(CustomUser customUser);
 }
